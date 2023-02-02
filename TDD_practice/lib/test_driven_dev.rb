62129bbc6498fdaa109d2717c95b5a727c935f37
Cory Boris <tromboris@gmail.com>
2023-02-02 14:43:58 -0500

    finish second problem

--- TDD_practice/lib/test_driven_dev.rb
+++ TDD_practice/lib/test_driven_dev.rb
@@ -1,5 +1,11 @@
 class Array 
     def my_uniq
-        
+        hash = Hash.new(0)
+        self.each {|ele| hash[ele] += 1}
+        return hash.keys
     end 
+
+    def two_sum
+        
+    end
 end 
--- TDD_practice/spec/test_driven_dev_spec.rb
+++ TDD_practice/spec/test_driven_dev_spec.rb
@@ -1,13 +1,24 @@
 require 'rspec'
 require 'test_driven_dev'
 
+
+# add exceptions down the road
 describe Array do 
-    describe 'my_uniq' do 
+    describe '#my_uniq' do 
         let(:arr) {[1,2,1,3,3]}
         it "#my_uniq should return unique elements in a new array " do 
             expect(arr.my_uniq).to eq([1,2,3])
-        end 
-    end 
+        end
+    end
 
+    describe '#two_sum' do
+        # let(:arr) {[-1,0,2,-2,1]}
+        context 'size less than 2' do
+            it 'should return empty array' do
+                expect([]).to eq([])
+                expect([0]).to eq([])
+            end
+        end
+    end
 
 end 
