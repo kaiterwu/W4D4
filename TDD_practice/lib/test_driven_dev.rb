586e92d04f02ae59e678d1741965bc454ca50c3a
Kaiter Wu <wukaiter@gmail.com>
2023-02-02 15:07:02 -0500

    finishg two_sum specs and lib

--- TDD_practice/lib/test_driven_dev.rb
+++ TDD_practice/lib/test_driven_dev.rb
@@ -6,6 +6,14 @@ class Array
     end 
 
     def two_sum
-        
+        raise TypeError if self.any?{|ele| !ele.is_a?(Integer)}
+        return [] if self.size<2 
+        arr =[] 
+        self.each_with_index do |ele1,i|
+            self.each_with_index do |ele2,j|
+                arr << [i,j].sort if ele1+ele2 == 0 && i<j 
+            end 
+        end 
+        arr 
     end
 end 
--- TDD_practice/spec/test_driven_dev_spec.rb
+++ TDD_practice/spec/test_driven_dev_spec.rb
@@ -13,12 +13,31 @@ describe Array do
 
     describe '#two_sum' do
         # let(:arr) {[-1,0,2,-2,1]}
-        context 'size less than 2' do
-            it 'should return empty array' do
-                expect([]).to eq([])
-                expect([0]).to eq([])
+        context 'self is an array' do 
+            context 'size less than 2' do
+                it 'should return empty array' do
+                    expect([].two_sum).to eq([])
+                    expect([0].two_sum).to eq([])
+                end
             end
-        end
+
+            context 'size greater than 2' do 
+                it 'should return 2d array with subarray of indices' do 
+                    arr = [-1,0,2,-2,1]
+                    expect(arr.two_sum).to eq([[0,4],[2,3]])
+                end 
+            end 
+        end 
+
+        context 'elemnt of array is not an integer' do 
+            it 'should raise TypeError' do 
+                arr = ["apple"]
+                expect{arr.two_sum}.to raise_error(TypeError)
+            end
+        end 
+
+    
+
     end
 
 end 
